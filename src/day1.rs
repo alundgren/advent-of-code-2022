0d9f1c98afbe3f519e975e90ad9b6c13282358b8
Andreas Lundgren <alundgren@users.noreply.github.com>
2022-12-01 09:34:34 +0000

    formatter

--- src/day1.rs
+++ src/day1.rs
@@ -21,33 +21,29 @@ fn iterate_numbers(filename: impl AsRef<Path>, f: &mut dyn FnMut(Option<i64>)) {
 }
 
 fn get_max_calories(filename: impl AsRef<Path>) -> i64 {
-	let mut max_elf_calories = 0;
-	let mut current_elf_calories = 0;
-    iterate_numbers(filename, &mut|calorie_count| {
-        match calorie_count {
-            Some(x) => current_elf_calories += x,
-            None  => {
-                if current_elf_calories > max_elf_calories {
-                    max_elf_calories = current_elf_calories;
-                }                
-                current_elf_calories = 0;                
+    let mut max_elf_calories = 0;
+    let mut current_elf_calories = 0;
+    iterate_numbers(filename, &mut |calorie_count| match calorie_count {
+        Some(x) => current_elf_calories += x,
+        None => {
+            if current_elf_calories > max_elf_calories {
+                max_elf_calories = current_elf_calories;
             }
-        }        
+            current_elf_calories = 0;
+        }
     });
     max_elf_calories
 }
 
 fn get_top_three_max_calories_sum(filename: impl AsRef<Path>) -> i64 {
     let mut elf_calories = Vec::<i64>::new();
-	let mut current_elf_calories = 0;
-    iterate_numbers(filename, &mut|calorie_count| {
-        match calorie_count {
-            Some(x) => current_elf_calories += x,
-            None  => {
-                elf_calories.push(current_elf_calories);
-                current_elf_calories = 0;                
-            }
-        }        
+    let mut current_elf_calories = 0;
+    iterate_numbers(filename, &mut |calorie_count| match calorie_count {
+        Some(x) => current_elf_calories += x,
+        None => {
+            elf_calories.push(current_elf_calories);
+            current_elf_calories = 0;
+        }
     });
     elf_calories.sort_by(|a, b| b.cmp(a));
     elf_calories[0..3].iter().sum()
@@ -56,23 +52,35 @@ fn get_top_three_max_calories_sum(filename: impl AsRef<Path>) -> i64 {
 pub fn part1_real_input() {
     let max_calories = get_max_calories("/workspaces/vscode-remote-try-rust/src/day1_input.txt");
     println!("{max_calories}");
-    assert!(max_calories == 68787, "Expected 68787 but got {}", max_calories);
+    assert!(
+        max_calories == 68787,
+        "Expected 68787 but got {}",
+        max_calories
+    );
 }
 
 pub fn part1_example_input() {
-    let max_calories = get_max_calories("/workspaces/vscode-remote-try-rust/src/day1_example_input.txt");
+    let max_calories =
+        get_max_calories("/workspaces/vscode-remote-try-rust/src/day1_example_input.txt");
     println!("{max_calories}");
-    assert!(max_calories == 24000, "Expected 24000 but got {}", max_calories);
+    assert!(
+        max_calories == 24000,
+        "Expected 24000 but got {}",
+        max_calories
+    );
 }
 
 pub fn part2_example_input() {
-    let result = get_top_three_max_calories_sum("/workspaces/vscode-remote-try-rust/src/day1_example_input.txt");
+    let result = get_top_three_max_calories_sum(
+        "/workspaces/vscode-remote-try-rust/src/day1_example_input.txt",
+    );
     println!("{result}");
     assert!(result == 45000, "Expected 45000 but got {}", result);
 }
 
 pub fn part2_real_input() {
-    let result = get_top_three_max_calories_sum("/workspaces/vscode-remote-try-rust/src/day1_input.txt");
+    let result =
+        get_top_three_max_calories_sum("/workspaces/vscode-remote-try-rust/src/day1_input.txt");
     println!("{result}");
     //assert!(result == 45000, "Expected 45000 but got {}", result);
 }
