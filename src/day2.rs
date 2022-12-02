dc1a22c3c927d933666e849acd03b0b81b9cb080
Andreas Lundgren <alundgren@users.noreply.github.com>
2022-12-02 08:28:01 +0000

    day2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,27 +1,64 @@
 use std::path::Path;
 use utils;
 
+const MY_RPS : [char; 3] = ['X', 'Y', 'Z'];
+
 pub fn part1_example_input() {
-    let total_points = get_total_points("/workspaces/advent-of-code-2022/src/day2_example_input.txt");
+    let total_points = get_total_points("/workspaces/advent-of-code-2022/src/day2_example_input.txt", false);
     println!("{}", total_points.to_string());    
 }
 
 pub fn part1_real_input() {
-    let total_points = get_total_points("/workspaces/advent-of-code-2022/src/day2_input.txt");
+    let total_points = get_total_points("/workspaces/advent-of-code-2022/src/day2_input.txt", false);
     println!("{}", total_points.to_string());    
 }
 
-fn get_total_points(filename: impl AsRef<Path>) -> u32 {
+pub fn part2_example_input() {
+    let total_points = get_total_points("/workspaces/advent-of-code-2022/src/day2_example_input.txt", true);
+    println!("{}", total_points.to_string());    
+}
+
+pub fn part2_real_input() {
+    let total_points = get_total_points("/workspaces/advent-of-code-2022/src/day2_input.txt", true);
+    println!("{}", total_points.to_string());    
+}
+
+fn get_total_points(filename: impl AsRef<Path>, is_part2 : bool) -> u32 {
     let lines = utils::lines_from_file(filename).expect("Could not load lines");
     let mut total_points : u32 = 0;
     for line in lines {
         let opponent_choice = line.chars().nth(0).unwrap();
-        let my_choice = line.chars().nth(2).unwrap();
-        total_points += get_points(opponent_choice, my_choice);        
+        let mut my_choice = line.chars().nth(2).unwrap();
+        if is_part2 {
+            my_choice = get_my_choice(opponent_choice, my_choice);
+        }
+        total_points += get_points(opponent_choice, my_choice);
     }
     total_points
 }
 
+fn get_my_choice(opponent_choice: char, desired_outcome : char) -> char {
+    const DRAW : char = 'Y';
+    const WIN : char = 'Z';
+    const LOSE : char = 'X';
+    const OPPONENT_CHOICE_ROCK : char = 'A';
+    const OPPONENT_CHOICE_PAPER : char = 'B';
+    const OPPONENT_CHOICE_SCISSORS : char = 'C';
+    const MY_CHOICE_ROCK : char = 'X';
+    const MY_CHOICE_PAPER: char = 'Y';
+    const MY_CHOICE_SCISSORS: char = 'Z';    
+    
+    match opponent_choice {
+        _ if desired_outcome == DRAW => *MY_RPS.get((opponent_choice as u32 - 'A' as u32) as usize).unwrap(),
+        OPPONENT_CHOICE_ROCK if desired_outcome == WIN => MY_CHOICE_PAPER,
+        OPPONENT_CHOICE_ROCK if desired_outcome == LOSE => MY_CHOICE_SCISSORS,
+        OPPONENT_CHOICE_PAPER if desired_outcome == WIN => MY_CHOICE_SCISSORS,
+        OPPONENT_CHOICE_PAPER if desired_outcome == LOSE => MY_CHOICE_ROCK,
+        OPPONENT_CHOICE_SCISSORS if desired_outcome == WIN => MY_CHOICE_ROCK,
+        OPPONENT_CHOICE_SCISSORS if desired_outcome == LOSE => MY_CHOICE_PAPER,
+        _ => panic!("broken")
+    }
+}
 
 fn get_points(opponent_choice: char, my_choice: char) -> u32 {
     //1 for Rock, 2 for Paper, and 3 for Scissors
--- src/main.rs
+++ src/main.rs
@@ -11,5 +11,7 @@ fn main() {
     println!("day2");
     day2::part1_example_input();
     day2::part1_real_input();
+    day2::part2_example_input();
+    day2::part2_real_input();
 }
 
